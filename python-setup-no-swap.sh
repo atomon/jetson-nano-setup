3365e8c58c0260dbfda2aee1cb6add5b68be8b7a
cycling-Ohnishi <robosystem_126@yahoo.co.jp>
2019-06-25 01:10:52 +0900

    add no-swap vertion

---
+++ python-setup-no-swap.sh
@@ -0,0 +1,22 @@
+#Install same library
+
+sudo apt-get update
+
+# Install and upgrade pip3
+sudo apt-get install python3-pip
+sudo pip3 install -U pip
+
+# Install python packages
+sudo apt-get install python3-pil
+sudo apt-get install python3-matplotlib
+
+# Install jupyter
+sudo pip3 install jupyter
+
+# Install avahi daemon for .local access
+sudo apt-get install avahi-daemon
+
+#Install GPU Graph
+git clone https://github.com/JetsonHacksNano/gpuGraph
+
+sudo apt-get upgrade
