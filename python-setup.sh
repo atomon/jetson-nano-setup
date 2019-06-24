abf3182686ff0b6b88437366b90babd9535150ff
cycling-Ohnishi <robosystem_126@yahoo.co.jp>
2019-06-24 23:46:57 +0900

    Install python same library command

---
+++ python-setup.sh
@@ -0,0 +1,19 @@
+#Install same library
+
+sudo apt-get update
+
+# Install and upgrade pip3
+sudo apt-get install python3-pip
+sudo pip3 install -U pip
+
+# Install python packages
+sudo apt-get install python3-pil
+sudo apt-get install python3-matplotlib
+
+# Install jupyter
+sudo pip3 install jupyter
+
+# Install avahi daemon for .local access
+sudo apt-get install avahi-daemon
+
+sudo apt-get upgrade
